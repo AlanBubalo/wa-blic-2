fef0784ac5e1a34c689efecc49b38a9c434b9726
Alan Bubalo <alan.buba5@gmail.com>
2022-11-22 08:41:06 +0100

    Get brand done

--- index.js
+++ index.js
@@ -30,6 +30,30 @@ app.post("/saveItem", async (req, res) => {
     }
 });
 
+// GET BRAND
+app.get("/getBrand", async (req, res) => {
+    let db = await connect("wa-blic-2");
+    let brand = req.query["brand"];
+    console.log(req.query);
+    let query = { brand: brand };
+    let cursor = await db.collection("collection").find(query);
+    let items = await cursor.toArray();
+    if (items) {
+        res.json({
+            status: "OK",
+            data: {
+                brand: brand,
+                items: items,
+            },
+        });
+    } else {
+        res.json({
+            status: "Failed",
+            message: `Brand ${brand} no found in DB`,
+        });
+    }
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
