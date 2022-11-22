995921b64faa23c555183e09f50ff9d8359b4509
Alan Bubalo <alan.buba5@gmail.com>
2022-11-22 08:50:04 +0100

    Get brand fixed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.post("/saveItem", async (req, res) => {
     let { name, price, brand } = req.body;
     let item = await db.collection("collection").insertOne({
         name: name,
-        price: ~~price,
+        price: parseFloat(price),
         brand: brand,
     });
     if (item) {
@@ -34,22 +34,48 @@ app.post("/saveItem", async (req, res) => {
 app.get("/getBrand", async (req, res) => {
     let db = await connect("wa-blic-2");
     let brand = req.query["brand"];
-    console.log(req.query);
     let query = { brand: brand };
     let cursor = await db.collection("collection").find(query);
     let items = await cursor.toArray();
+    let items_filtered = items.map((item) => {
+        return {
+            name: item.name,
+            price: item.price,
+        };
+    });
     if (items) {
         res.json({
             status: "OK",
             data: {
                 brand: brand,
-                items: items,
+                items: items_filtered,
             },
         });
     } else {
         res.json({
             status: "Failed",
             message: `Brand ${brand} no found in DB`,
+        });
+    }
+});
+
+// GET ITEM BY ID
+app.get("/getItemById", async (req, res) => {
+    let db = await connect("wa-blic-2");
+    let id = req.query["id"];
+    let query = { _id: mongo.ObjectId(id) };
+    let item = await db.collection("collection").findOne(query);
+    if (item) {
+        res.json({
+            status: "OK",
+            data: {
+                item: item,
+            },
+        });
+    } else {
+        res.json({
+            status: "Failed",
+            message: `Couldn't find item by id ${id}`,
         });
     }
 });
